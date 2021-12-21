17915dff15f60b90a250dd7c35b760d9c3db4521
s-jvidal <jonasvidal771@gmail.com>
2021-12-21 14:53:50 +0100

    Kalman Positions can be read via P2P now

--- examples/app_peer_to_peer/src/peer_to_peer.c
+++ examples/app_peer_to_peer/src/peer_to_peer.c
@@ -46,25 +46,39 @@
 #define MESSAGE "hello world"
 #define MESSAGE_LENGHT 11
 
+// own includes
+#include "estimator_kalman.h"
+#include "stabilizer_types.h"
+
+point_t my_data; 
+struct payload {
+  float x;
+  float y;
+  float z;
+} payload;
 
 void p2pcallbackHandler(P2PPacket *p)
 {
   // Parse the data from the other crazyflie and print it
   uint8_t other_id = p->data[0];
-  static char msg[MESSAGE_LENGHT + 1];
-  memcpy(&msg, &p->data[1], sizeof(char)*MESSAGE_LENGHT);
-  msg[MESSAGE_LENGHT] = 0;
+  static struct payload msg;
+  memcpy(&msg,&p->data[1],sizeof(msg));
+  //static char msg[MESSAGE_LENGHT + 1];
+  //memcpy(&msg, &p->data[1], sizeof(char)*MESSAGE_LENGHT);
+  //msg[MESSAGE_LENGHT] = 0;
   uint8_t rssi = p->rssi;
 
-  DEBUG_PRINT("[RSSI: -%d dBm] Message from CF nr. %d, %s\n", rssi, other_id, msg);
+  DEBUG_PRINT("[RSSI: -%d dBm] Message from CF nr. %d, %f , %f, %f  \n", rssi, other_id, (double)msg.x ,(double) msg.y , (double) msg.z);
 }
 
 void appMain()
 {
   DEBUG_PRINT("Waiting for activation ...\n");
-
+DEBUG_PRINT("X: %.3f, Y: %.3f, Z: %.3f\n", (double) my_data.x,(double) my_data.y, (double) my_data.z);
     // Initialize the p2p packet 
     static P2PPacket p_reply;
+    
+
     p_reply.port=0x00;
     
     // Get the current address of the crazyflie and obtain
@@ -75,11 +89,10 @@ void appMain()
     p_reply.data[0]=my_id;
 
     //Put a string in the payload
-    char *str="Hello World";
-    memcpy(&p_reply.data[1], str, sizeof(char)*MESSAGE_LENGHT);
+    //char *str="Hello World";
+    
 
-    // Set the size, which is the amount of bytes the payload with ID and the string 
-    p_reply.size=sizeof(char)*MESSAGE_LENGHT+1;
+    
 
     // Register the callback function so that the CF can receive packets as well.
     p2pRegisterCB(p2pcallbackHandler);
@@ -89,9 +102,16 @@ void appMain()
     //   Note: if they are sending at the exact same time, there will be message collisions, 
     //    however since they are sending every 2 seconds, and they are not started up at the same
     //    time and their internal clocks are different, there is not really something to worry about
-
     vTaskDelay(M2T(2000));
+estimatorKalmanGetEstimatedPos(&my_data);
+payload.x = my_data.x;
+payload.y = my_data.y;
+payload.z = my_data.z;
+memcpy(&p_reply.data[1], &payload, sizeof(payload));
+// Set the size, which is the amount of bytes the payload with ID and the string 
+    p_reply.size=sizeof(payload)+1; 
     radiolinkSendP2PPacketBroadcast(&p_reply);
+    DEBUG_PRINT("X: %f,Y: %f , Z: %f\n", (double) payload.x,(double) payload.y,(double) payload.z);
   }
 }
 
